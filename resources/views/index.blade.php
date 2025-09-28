0c56798c35f8d8c0d299e20bbbfff210b17169ad
rusdee22 <rusdee4027s@gmail.com>
2025-09-28 14:07:48 +0700

    add text

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <h1>หลักสูตร</h1>
     <hr>
-    <a href="{{ route('course') }}" class="btn btn-primary mb-3">+เพิ่มหลักสูตร</a>
+    <a href="{{ route('course') }}" class="btn btn-primary mb-3">++++เพิ่มหลักสูตร</a>
+    <h1>iuu</h1>
     @if(section('success'))
     <div class="alert alert-success">{{ session('success') }}</div>
     @endif
